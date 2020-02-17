77bfcec411885098c7f234830dde29bba4d32d58
Anant Mathur <anant.mathur007@gmail.com>
2020-02-17 08:59:10 +0530

    final commit

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended:true}));
-
-mongoose.connect("mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/test?retrywrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/hackvsit",{useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/test?retrywrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use('/',require('./routes/index'));
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 5000, function(){
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
